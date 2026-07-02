535417e10bc9bdedf67c9eb82a88f9b13314294a
rodrilang <rodrigolang90mail.com>
2026-07-02 14:25:54 -0300

    health

--- src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
+++ src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-
 @NullMarked
 @Configuration
 public class CorsConfig {
