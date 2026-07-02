1284592637908fe57a1f959cde1767b14a2250ef
rodrilang <rodrigolang90mail.com>
2026-07-02 11:28:13 -0300

    deploy configuration

--- src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
+++ src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
@@ -1,14 +1,20 @@
 package com.rodrilang.librarymanager.config;
 
 import org.jspecify.annotations.NullMarked;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @NullMarked
 @Configuration
 public class CorsConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -17,7 +23,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(String.valueOf(Arrays.asList(allowedOrigins)))
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
