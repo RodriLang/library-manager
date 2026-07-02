ff7fec82fde3035aed50a407cc0734897b077931
rodrilang <rodrigolang90mail.com>
2026-07-02 12:43:16 -0300

    cofiguracion cors

--- src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
+++ src/main/java/com/rodrilang/librarymanager/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
